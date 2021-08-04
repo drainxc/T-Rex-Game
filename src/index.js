c37825ee861f437ba804b918bc5ab5d856a852e7
ldh7228 <ldh7228@gmail.com>
2021-08-04 15:35:52 +0900

    Add TRex

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 1520;
-canvas.height = 600;
+canvas.width = 700;
+canvas.height = 300;
 
 let TRex = new Image();
 let cactus = new Image();
 
+let TRexX = 50;
+let TRexY = 225;
 let jump = false;
 let bow = false;
+let move = 1;
 
 let TRexImg = [
     "../asset/img/T-Rex.png",
@@ -31,6 +34,17 @@ let cactusImg = [
     "../asset/img/bigCactus5.png",
 ];
 
+setInterval (function () {
+    TRex.src = TRexImg[move];
+    draw();
+    if (move == 1) {
+        move++;
+    }
+    else if (move == 2) {
+        move--;
+    }
+}, 100);
+
 function keyEvent(event) {
     console.log(event.key);
     if (event.key == ' ') {
@@ -42,7 +56,11 @@ function keyEvent(event) {
     if (event.key == 'ArrowDown') {
         bow = true;
     }
-    
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(TRex, TRexX, TRexY);
 }
 
 document.addEventListener('keydown', keyEvent);
