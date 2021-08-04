5d44fa85bf8b9372e30325e56c09dae6e0aaba17
ldh7228 <ldh7228@gmail.com>
2021-08-04 15:25:36 +0900

    Add key event

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById('canvas');
-const ctx =  canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 canvas.width = 1520;
 canvas.height = 600;
@@ -7,12 +7,15 @@ canvas.height = 600;
 let TRex = new Image();
 let cactus = new Image();
 
+let jump = false;
+let bow = false;
+
 let TRexImg = [
     "../asset/img/T-Rex.png",
     "../asset/img/T-RexMove1.png",
     "../asset/img/T-RexMove2.png",
     "../asset/img/DeadT-Rex.png"
-]
+];
 
 let cactusImg = [
     "../asset/img/cactus1.png",
@@ -26,4 +29,20 @@ let cactusImg = [
     "../asset/img/bigCactus3.png",
     "../asset/img/bigCactus4.png",
     "../asset/img/bigCactus5.png",
-]
+];
+
+function keyEvent(event) {
+    console.log(event.key);
+    if (event.key == ' ') {
+        jump = true;
+    }
+    if (event.key == 'ArrowUp') {
+        jump = true;
+    }
+    if (event.key == 'ArrowDown') {
+        bow = true;
+    }
+    
+}
+
+document.addEventListener('keydown', keyEvent);
